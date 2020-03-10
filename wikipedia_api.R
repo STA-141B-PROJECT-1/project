3007ee4e632d0b4e599757c87068d31f254dbeeb
ahui187 <ahui187@gmail.com>
2020-03-09 20:53:22 -0700

    improved content output and added WikpediR package option

--- wikipedia_api.R
+++ wikipedia_api.R
@@ -2,12 +2,14 @@ library(tidyverse)
 library(jsonlite)
 library(httr)
 library(rvest)
+library(WikipediR)
 
+#scraping content of Wikipedia page using API
 r <- GET("https://en.wikipedia.org/w/api.php",
           query = list(
             action = "parse",
             page = "Steven Adams",
-            prop = "wikitext",
+            prop = "text",
             format = "json"
           ))
 
@@ -15,5 +17,14 @@ stop_for_status(r)
 
 json <- content(r, as = "text")
 test <- fromJSON(json)
-test$parse$wikitext %>% 
+test$parse$text$`*` %>% 
+  read_html() %>% 
+  html_text() %>% 
+  str_remove_all("\n")
+
+#scraping content of Wikipedia page using WikipediaR package
+wiki = page_content(language = "en", "wikipedia", page_name = "Steven Adams")
+wiki$parse$text$'*' %>% 
+  read_html() %>% 
+  html_text() %>% 
   str_remove_all("\n")
